dbc0cce1736094870aba4d27feafae7d4a802411
EkoAgustina <eko.agustina86@gmail.com>
2023-11-25 19:09:14 +0700

    Added handling DesiredCapabilities for iOS

--- src/test/java/Capabilities/BasicClass.java
+++ src/test/java/Capabilities/BasicClass.java
@@ -1,33 +1,46 @@
 package Capabilities;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import mappings.mapper;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-
-import java.io.File;
-import java.net.URL;
-
-import static mappings.mapper.file_path;
 import static mappings.mapper.key_apps;
 
 public class BasicClass {
+    /*
+      Desired Capabilities is used to send JSON Data to Appium Server
+    */
     public static DesiredCapabilities initDevice(){
-        /*
-          Desired Capabilities is used to send JSON Data to Appium Server
-         */
+        
         DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability("automationName","UiAutomator2");
-        cap.setCapability("platformName","Android");
-        cap.setCapability("deviceName",System.getenv("deviceName"));
-        cap.setCapability("autoGrantPermissions","true");
-        cap.setCapability("noReset","false");
-        cap.setCapability("printPageSourceOnFindFailure","true");
-        cap.setCapability("app", key_apps(System.getenv("apps")));
-        cap.setCapability("appWaitDuration","30000");
-        cap.setCapability("appWaitActivity","SplashActivity, SplashActivity,OtherActivity, *, *.SplashActivity");
+        String platformType = System.getenv("platform");
+
+        switch (platformType) {
+            case "android":
+                cap.setCapability("automationName","UiAutomator2");
+                cap.setCapability("platformName","Android");
+                cap.setCapability("deviceName",System.getenv("deviceName"));
+                cap.setCapability("autoGrantPermissions","true");
+                cap.setCapability("noReset","false");
+                cap.setCapability("printPageSourceOnFindFailure","true");
+                cap.setCapability("app", key_apps(System.getenv("apps")));
+                cap.setCapability("appWaitDuration","30000");
+                cap.setCapability("appWaitActivity","SplashActivity, SplashActivity,OtherActivity, *, *.SplashActivity"); 
+                break;
+            case "ios":
+                cap.setCapability("automationName","XCUITest");
+                cap.setCapability("platformName","iOS");
+                cap.setCapability("deviceName",System.getenv("deviceName"));
+                cap.setCapability("udid", System.getenv("iosUdid"));
+                cap.setCapability("autoGrantPermissions","true");
+                cap.setCapability("noReset","false");
+                cap.setCapability("printPageSourceOnFindFailure","true");
+                cap.setCapability("app", key_apps(System.getenv("apps")));
+                cap.setCapability("appWaitDuration","30000");
+                cap.setCapability("appWaitActivity","SplashActivity, SplashActivity,OtherActivity, *, *.SplashActivity");
+                break;
+            default:
+                throw new Error("Platform not recognized");
+        }
+        
 
         return cap;
     }
