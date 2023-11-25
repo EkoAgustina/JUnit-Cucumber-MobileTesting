88354057085f1713304c03563658cb66c73b8dac
EkoAgustina <eko.agustina86@gmail.com>
2023-11-25 08:09:34 +0700

    Added runner sh

---
+++ runner.sh
@@ -0,0 +1,31 @@
+platform=${1}
+
+appiumPort='4725'
+
+# Android setup
+apps='wdio.apk'
+deviceName='Pixel4_12'
+
+# iOS setup
+apps='swaglabs.app'
+deviceName='iPhone_15'
+iosUdid='EC865F6C-9547-4290-9942-B2A83F91B81C'
+
+cucumberTags='@validAccount'
+
+
+export platform=${platform}
+export deviceName=${deviceName}
+export iosUdid=${iosUdid}
+export apps=${apps}
+export cucumberTags=${cucumberTags}
+export appiumPort=${appiumPort}
+
+
+appium --address 0.0.0.0 --port $appiumPort&
+P1=$!
+mvn clean test -Dcucumber.filter.tags=$cucumberTags&
+P2=$!
+
+wait $P2
+pkill -9 -f $appiumPort
